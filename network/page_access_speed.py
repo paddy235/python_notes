6cb2f36480f0a8711f3a48f136f25802067a087d
dandh811 <dandh811@163.com>
2020-09-27 15:51:59 +0800

    1

--- network/page_access_speed.py
+++ network/page_access_speed.py
@@ -1,3 +1,9 @@
+"""
+需求分析：与LDAP服务器建立连接，查询公司所有的员工信息和部门信息，并将结果保存在2个json文件中。
+运行环境：python3 + Windows或者Linux
+依赖库第三方库： ldap3
+GitHub链接：https://github.com/dandh811/python_notes/blob/master/networkServices/LDAP/ldap_tool.py
+"""
 import requests
 import logging
 import time
